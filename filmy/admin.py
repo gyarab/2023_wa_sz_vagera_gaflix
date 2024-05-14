a4e3dd28a61fda04c4dd32e784f161eb7079ceb1
JaVa <94869479+JakubVagera@users.noreply.github.com>
2024-05-14 21:22:38 +0200

    Add files via upload

---
+++ filmy/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from filmy.models import Movie, Director, Genre, Actor
+
+# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+class DirectorAdmin(admin.ModelAdmin):
+    pass
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Movie, MovieAdmin)
+
+admin.site.register(Director, DirectorAdmin)
+
+admin.site.register(Genre, GenreAdmin)
+
+admin.site.register(Actor, ActorAdmin)
+# Register your models here.
+
+
+
---
+++ filmy/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+class Movie(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text="in minutes")
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    name = models.CharField(max_length=300)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+class Actor(models.Model):
+    name = models.CharField(max_length=300)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    main_picture = models.ImageField(blank=True)
+    description = models.TextField(blank=True) 
+    
+    def __str_(self):
+        return self.name       
+
+
+
